985827baad06e76d24ba0979bab33a33f633b2db
jeadie <jack@spice.ai>
2023-09-17 11:28:18 +1000

    fix serde/xlm problems

--- src/s3api.rs
+++ src/s3api.rs
@@ -1,15 +1,16 @@
 use std::io::Read;
-use quick_xml::reader::Reader;
+use serde_xml_rs::de::from_reader;
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
 
-use serde::{self, de::DeserializeOwned};
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 struct CompleteMultipartUpload {
     #[serde(rename = "Part")]
     parts: Vec<Part>,
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Part {
     #[serde(rename = "PartNumber")]
     part_number: i32,
@@ -17,26 +18,27 @@ struct Part {
     e_tag: String,
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Delete {
     #[serde(rename = "Object")]
     objects: Vec<Object>,
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Object {
     #[serde(rename = "Key")]
     key: String,
 }
 
-fn parse_xml<T: DeserializeOwned, R: Read>(reader: R) -> Result<T, quick_xml::Error> {
-    Reader::from_reader(reader)
+fn parse_xml<T: DeserializeOwned, R: Read>(reader: R) -> Result<T, serde_xml_rs::Error> {
+
+    from_reader(reader)
 }
 
-fn parse_complete_multipart_upload<R: Read>(reader: R) -> Result<CompleteMultipartUpload, quick_xml::Error> {
+fn parse_complete_multipart_upload<R: Read>(reader: R) -> Result<CompleteMultipartUpload, serde_xml_rs::Error> {
     parse_xml(reader)
 }
 
-fn parse_delete<R: Read>(reader: R) -> Result<Delete, quick_xml::Error> {
+fn parse_delete<R: Read>(reader: R) -> Result<Delete, serde_xml_rs::Error> {
     parse_xml(reader)
 }
